f581f81edf8010bd15d25e054c2e373276dc1235
DionataBergmann <dionata.podewils@outlook.com>
2022-05-24 18:06:30 -0300

    :sparkles: initial project base

---
+++ app/common/theme/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  primary: '#64AF46',
+  secundary: '#FFFFFF',
+  tertiary: '#548049',
+  white: '#FFFFFF',
+}
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+// import { GetServerSideProps } from 'next'
+
+export default function Index() {
+  return <></>
+}
